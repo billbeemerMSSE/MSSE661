2564e53a2b80857e4daac7ce5f5d25ca26156889
Bill Beemer <90537133+billbeemerMSSE@users.noreply.github.com>
2023-04-14 17:28:26 -0700

    Revert "WIP week 5 homework"

--- MS661_ClassWebServer/restful-api-mysql/src/routes/auth.routes.js
+++ MS661_ClassWebServer/restful-api-mysql/src/routes/auth.routes.js
@@ -1,27 +1,10 @@
 const express = require("express");
-
-// const controller = require("../controllers/auth.controller");
-
-const {
-    registerUser,
-    loginUser,
-    token,
-    logoutUser,
-} = require("../controllers/auth.controller");
+const controller = require("../controllers/auth.controller");
 
 const authRoutes = express.Router();
 
-// authRoutes.post("/register", controller.registerUser);
+authRoutes.post("/register", controller.registerUser);
 
-// authRoutes.post("/login", controller.loginUser);
-
-authRoutes.post("/register", registerUser);
-
-authRoutes.post("/login", loginUser);
-
-authRoutes.post("/token", token);
-
-authRoutes.post("/logout", logoutUser);
+authRoutes.post("/login", controller.loginUser);
 
 module.exports = authRoutes;
-
