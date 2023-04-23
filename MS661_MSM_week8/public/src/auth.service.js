b59696eee2a493735e4819c1feaf576b584959c5
Bill Beemer <90537133+billbeemerMSSE@users.noreply.github.com>
2023-04-23 00:28:30 -0700

    Revert "Develop"

---
+++ MS661_MSM_week8/public/src/auth.service.js
@@ -0,0 +1,7 @@
+const register = (formData) => _post(`${BASE_API_URL}/register`, formData);
+const login = (formData) => _post(`${BASE_API_URL}/login`, formData);
+
+const logout = () => {
+    clearStorage('isAuth');
+    clearStorage('token');
+};
